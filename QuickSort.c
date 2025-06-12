ca5788042a07b56ce96863ba90ee3b3306696ef5
AniquaGhayas <aniquaghayas@gmail.com>
2025-06-12 20:22:03 +0530

    QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,59 +1,68 @@
 #include <stdio.h>
 
-void swap(int* a, int* b)
-{
+// Function to swap two elements
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++)
-	{
-        if (arr[j] < pivot)
-		{
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
-}
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
 
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-	{
+    while (i <= j) {
+        while (i <= high && arr[i] <= pivot)
+            i++;
+        while (arr[j] > pivot)
+            j--;
+        if (i < j)
+            swap(&arr[i], &arr[j]);
+    }
+
+    swap(&arr[low], &arr[j]);  // Place pivot in correct position
+    return j;
+}
+ 
+// QuickSort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
         int pi = partition(arr, low, high);
+
+        // Recursively sort elements before and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-void printArray(int arr[], int size)
-{
+// Function to print array
+void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-int main()
-{
+int main() {
     int n;
+
     printf("Enter number of elements: ");
     scanf("%d", &n);
+
     int arr[n];
+
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
+
     printf("Original array:\n");
     printArray(arr, n);
+
     quickSort(arr, 0, n - 1);
+
     printf("Sorted array:\n");
     printArray(arr, n);
+
     return 0;
 }
-
